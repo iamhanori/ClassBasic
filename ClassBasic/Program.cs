e2585170b8e6eb9374d59b8ae170a992a08d02ca
Hanul Kim <s2122@e-mirim.hs.kr>
2023-05-08 12:19:14 +0900

    Class 메서드

--- ClassBasic/Program.cs
+++ ClassBasic/Program.cs
@@ -4,8 +4,18 @@ namespace NewClassProgram3204 // Note: actual namespace depends on the project n
 {
     internal class Program
     {
+        private class Math {
+            /*static public int Abs(int a)
+            {
+                return 0;
+            }*/
+
+        }
         static void Main(string[] args)
         {
+            Math math = new Math();
+            Console.WriteLine(Math.Abs(-10));
+
             ThirdClass abc;
             ABCClass abc2;
             InnerClass inn;
